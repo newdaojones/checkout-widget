2df7904329c538d2f24d751d1eeb96651aa5cac6
pei <peipeilong116@gmail.com>
2023-01-19 23:08:36 -0500

    feat: build transaction details and card details page

--- src/pages/tipAndSubTotal.tsx
+++ src/pages/tipAndSubTotal.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import UsFlagImage from '../assets/images/us-flag.png'
+import { CheckoutInfo } from "../types/checkout.type";
 
-export const TipAndSubTotal = ({ cost, tipPercent, onChangeCost, onChangeTipPercent, onNext }: {
-  cost?: number,
-  tipPercent?: number,
-  onChangeTipPercent: (value: number) => void
-  onChangeCost: (value: number) => void
+export const TipAndSubTotal = ({ checkoutInfo, onChange, onNext }: {
+  checkoutInfo: CheckoutInfo,
+  onChange: (key: string, value: any) => void
   onNext: () => void
 }) => {
   const [focusedCustomTip, setFocusedCustomTip] = useState(false)
@@ -13,7 +12,7 @@ export const TipAndSubTotal = ({ cost, tipPercent, onChangeCost, onChangeTipPerc
     const value = e.target.value;
 
     if (!Number.isNaN(Number(value))) {
-      onChangeCost(value)
+      onChange('cost', value)
     }
   }
 
@@ -21,7 +20,7 @@ export const TipAndSubTotal = ({ cost, tipPercent, onChangeCost, onChangeTipPerc
     const value = e.target.value;
 
     if (!Number.isNaN(Number(value))) {
-      onChangeTipPercent(value)
+      onChange('tipPercent', value)
     }
   }
 
@@ -30,7 +29,7 @@ export const TipAndSubTotal = ({ cost, tipPercent, onChangeCost, onChangeTipPerc
     <p className="text-white text-lg text-left">Cost of Goods</p>
     <div className="flex w-full">
       <input
-        value={cost}
+        value={checkoutInfo.cost}
         onChange={onChangeCostInput}
         className="border-white outline-none border-2 rounded-md h-11 bg-transparent flex-1 text-white text-md text-right text-lg p-2 shadow-sm shadow-white"
       />
@@ -42,48 +41,48 @@ export const TipAndSubTotal = ({ cost, tipPercent, onChangeCost, onChangeTipPerc
     <div>
       <p className="text-white text-lg mt-4 text-left">+ Tip (optional)</p>
       <div className='mt-3 flex w-full justify-end items-center'>
-        <p className='text-white text-lg m-0 mr-5'> {cost ? (Number(cost) * 0.1).toFixed(2) : ''}</p>
+        <p className='text-white text-lg m-0 mr-5'> {checkoutInfo.cost ? (Number(checkoutInfo.cost) * 0.1).toFixed(2) : ''}</p>
         <div
-          className={`h-11 bg-white/50 text-white flex items-center justify-center w-24 rounded-md cursor-pointer shadow-md shadow-white ${tipPercent === 10 ? 'bg-gradient-to-b from-purple-200 to-purple-400' : ''}`}
-          onClick={() => onChangeTipPercent(10)}
+          className={`h-11 bg-white/50 text-white flex items-center justify-center w-24 rounded-md cursor-pointer shadow-md shadow-white ${checkoutInfo.tipPercent === '10' ? 'bg-gradient-to-b from-purple-200 to-purple-400' : ''}`}
+          onClick={() => onChange('pricePercent', '10')}
         >
           10%
         </div>
       </div>
       <div className='mt-3 flex w-full justify-end items-center'>
-        <p className='text-white text-lg m-0 mr-5'> {cost ? (Number(cost) * 0.15).toFixed(2) : ''}</p>
+        <p className='text-white text-lg m-0 mr-5'> {checkoutInfo.cost ? (Number(checkoutInfo.cost) * 0.15).toFixed(2) : ''}</p>
         <div
-          className={`h-11 bg-white/50 text-white flex items-center justify-center w-24 rounded-md cursor-pointer shadow-md shadow-white ${tipPercent === 15 ? 'bg-gradient-to-b from-purple-200 to-purple-400' : ''}`}
-          onClick={() => onChangeTipPercent(15)}
+          className={`h-11 bg-white/50 text-white flex items-center justify-center w-24 rounded-md cursor-pointer shadow-md shadow-white ${checkoutInfo.tipPercent === '15' ? 'bg-gradient-to-b from-purple-200 to-purple-400' : ''}`}
+          onClick={() => onChange('tipPercent', '15')}
         >
           15%
         </div>
       </div>
       <div className='mt-3 flex w-full justify-end items-center'>
-        <p className='text-white text-lg m-0 mr-5'> {cost ? (Number(cost) * 0.2).toFixed(2) : ''}</p>
+        <p className='text-white text-lg m-0 mr-5'> {checkoutInfo.cost ? (Number(checkoutInfo.cost) * 0.2).toFixed(2) : ''}</p>
         <div
-          className={`h-11 bg-white/50 text-white flex items-center justify-center w-24 rounded-md cursor-pointer shadow-md shadow-white ${tipPercent === 20 ? 'bg-gradient-to-b from-purple-200 to-purple-400' : ''}`}
-          onClick={() => onChangeTipPercent(20)}
+          className={`h-11 bg-white/50 text-white flex items-center justify-center w-24 rounded-md cursor-pointer shadow-md shadow-white ${checkoutInfo.tipPercent === '20' ? 'bg-gradient-to-b from-purple-200 to-purple-400' : ''}`}
+          onClick={() => onChange('tipPercent', '20')}
         >
           20%
         </div>
       </div>
       <div className='mt-3 flex w-full justify-end items-center'>
-        <p className='text-white text-lg m-0 mr-5'> {cost && tipPercent && ![10,15,20].includes(Number(tipPercent)) ? (Number(cost) * Number(tipPercent) / 100).toFixed(2) : ''}</p>
+        <p className='text-white text-lg m-0 mr-5'> {checkoutInfo.cost && checkoutInfo.tipPercent && ![10,15,20].includes(Number(checkoutInfo.tipPercent)) ? (Number(checkoutInfo.cost) * Number(checkoutInfo.tipPercent) / 100).toFixed(2) : ''}</p>
         {focusedCustomTip?
         <input
-          value={[10, 15, 20].includes(Number(tipPercent)) ? undefined : tipPercent}
+          value={[10, 15, 20].includes(Number(checkoutInfo.tipPercent)) ? undefined : checkoutInfo.tipPercent}
           placeholder="??%"
           autoFocus
           onBlur={() => setFocusedCustomTip(false)}
           onChange={onChangeCustomTipPercent}
-          className={`h-11 bg-white/50 text-center outline-none text-white flex items-center justify-center w-24 rounded-md cursor-pointer shadow-md shadow-white placeholder-white ${tipPercent && ![10,15,20].includes(Number(tipPercent)) ? 'bg-gradient-to-b from-purple-200 to-purple-400' : ''}`}
+          className={`h-11 bg-white/50 text-center outline-none text-white flex items-center justify-center w-24 rounded-md cursor-pointer shadow-md shadow-white placeholder-white ${checkoutInfo.tipPercent && !['10','15','20'].includes(checkoutInfo.tipPercent) ? 'bg-gradient-to-b from-purple-200 to-purple-400' : ''}`}
         />
         : <div
-        className={`h-11 bg-white/50 text-white flex items-center justify-center w-24 rounded-md cursor-pointer shadow-md shadow-white ${tipPercent && ![10,15,20].includes(Number(tipPercent)) ? 'bg-gradient-to-b from-purple-200 to-purple-400' : ''}`}
+        className={`h-11 bg-white/50 text-white flex items-center justify-center w-24 rounded-md cursor-pointer shadow-md shadow-white ${checkoutInfo.tipPercent && !['10','15','20'].includes(checkoutInfo.tipPercent) ? 'bg-gradient-to-b from-purple-200 to-purple-400' : ''}`}
         onClick={() => setFocusedCustomTip(true)}
       >
-        {tipPercent && ![10,15,20].includes(Number(tipPercent)) ? `${Number(tipPercent)}%` : '??%'}
+        {checkoutInfo.tipPercent && !['10','15','20'].includes(checkoutInfo.tipPercent) ? `${checkoutInfo.tipPercent}%` : '??%'}
       </div>
         }
       </div>
@@ -91,7 +90,7 @@ export const TipAndSubTotal = ({ cost, tipPercent, onChangeCost, onChangeTipPerc
     <p className="mt-2 text-white text-lg text-left">= Sub-Total</p>
     <div className="flex w-full">
       <div className="border-white border-2 rounded-md h-11 bg-transparent flex-1 text-white text-md text-right text-lg p-2 shadow-sm shadow-white">
-        {cost ? (Number(cost) + Number(cost) * Number(tipPercent || 0) / 100).toFixed(2) : ''}
+        {checkoutInfo.cost ? (Number(checkoutInfo.cost) + Number(checkoutInfo.cost) * Number(checkoutInfo.tipPercent || 0) / 100).toFixed(2) : ''}
       </div>
       <div className='border-2 border-white rounded-md h-11 w-24 ml-1 flex items-center justify-center text-white text-lg shadow-sm shadow-white'>
         <img src={UsFlagImage} alt='' className="flag mr-2" />
@@ -99,9 +98,9 @@ export const TipAndSubTotal = ({ cost, tipPercent, onChangeCost, onChangeTipPerc
       </div>
     </div>
     <button
-      disabled={!cost}
+      disabled={!checkoutInfo.cost}
       onClick={() => onNext()}
-      className={`mt-4 text-white text-lg text-center w-full rounded-md h-11 border-2 border-white flex items-center justify-center shadow-md shadow-white ${cost ? 'bg-gradient-to-b from-purple-400 to-purple-600' : ''}`}
+      className={`mt-4 text-white text-lg text-center w-full rounded-md h-11 border-2 border-white flex items-center justify-center shadow-md shadow-white ${checkoutInfo.cost ? 'bg-gradient-to-b from-purple-400 to-purple-600' : ''}`}
     >
       Get Total
     </button>
---
+++ src/pages/transactionDetails.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import UsFlagImage from '../assets/images/us-flag.png'
+import VisaIcon from '../assets/images/visa-icon.png'
+import { CheckoutInfo } from "../types/checkout.type";
+import moment from 'moment-timezone'
+
+export const TransactionDetails = ({ checkoutInfo, onNext }: {
+  checkoutInfo: CheckoutInfo,
+  onNext: () => void
+}) => {
+  const [confirmed, setConfirmed] = useState(false)
+  return (<div className='widget-container flex flex-col'>
+    <h3 className="text-white text-4xl mb-10 text-center">[tx-status]</h3>
+    <div className="flex-1 flex flex-col justify-between">
+      <div>
+        <p className="text-white text-lg text-left">Amount</p>
+        <div className="flex w-full">
+          <div className="border-white border-2 rounded-md h-11 bg-transparent flex-1 text-white text-md text-right text-lg p-2 shadow-sm shadow-white">
+            {checkoutInfo.cost ? (Number(checkoutInfo.cost) + Number(checkoutInfo.cost) * Number(checkoutInfo.tipPercent || 0) / 100).toFixed(2) : ''}
+          </div>
+          <div className='border-2 border-white rounded-md h-11 w-24 ml-1 flex items-center justify-center text-white text-lg shadow-sm shadow-white'>
+            <img src={UsFlagImage} alt='' className="flag mr-2" />
+            USD
+          </div>
+        </div>
+      </div>
+      <div>
+        <p className="text-white text-lg text-left">Payment Method</p>
+        <div className="flex w-full">
+          <div className="border-white border-2 rounded-md h-11 bg-transparent flex-1 text-white text-md text-right text-lg p-2 shadow-sm shadow-white">
+            Accepted
+          </div>
+          <div className='border-2 border-white rounded-md h-11 w-24 ml-1 flex items-center justify-center text-white text-lg shadow-sm shadow-white'>
+            <img src={VisaIcon} alt='' className="!w-8" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <p className="text-white text-lg text-left">Transaction Status</p>
+        <div className="flex w-full">
+          <div className="border-white border-2 rounded-md h-11 bg-transparent flex-1 text-white text-md text-center text-lg p-2 shadow-sm shadow-white">
+            0xbe53ca2 ... 9b77ccbf39
+          </div>
+          <div className='border-2 border-white rounded-md h-11 w-24 ml-1 flex items-center justify-center text-white text-lg shadow-sm shadow-white'>
+            ETH TX ID
+          </div>
+        </div>
+      </div>
+      <div>
+        <p className="text-white text-lg text-left">Date</p>
+        <div className="flex w-full">
+          <div className="border-white border-2 rounded-md h-11 bg-transparent flex-1 text-white text-md text-lg p-2 shadow-sm shadow-white text-center">
+            {moment().format('MM DD YYYY')}
+          </div>
+          <div className='border-2 border-white rounded-md h-11 w-24 ml-1 flex items-center justify-center text-white text-lg shadow-sm shadow-white'>
+            {moment().format('HH:MM')}
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div className="mt-6 mb-2 text-left">
+      <label className="text-white text-xs cursor-pointer select-none">
+        <input className="checkbox" type="checkbox" checked={confirmed} onChange={(e) => setConfirmed(e.target.checked)} />
+        Check this box after you confirm that a copy of this receipt has been sent to your inbox
+      </label>
+    </div>
+    <button
+      onClick={() => onNext()}
+      className={`mt-4 text-white text-lg text-center w-full rounded-md h-11 border-2 border-white flex items-center justify-center shadow-md shadow-white ${confirmed ? 'bg-gradient-to-b from-purple-400 to-purple-600' : ''}`}
+    >
+      Close View
+    </button>
+  </div>)
+}
