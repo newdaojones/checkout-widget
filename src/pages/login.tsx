c895847a14c4affe6e5760ba1ca940d97b9d74c1
pei <peipeilong116@gmail.com>
2023-05-08 21:39:50 -0400

    feat: add us patriot act text

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,74 +4,115 @@ import { CheckoutInfo } from "../types/checkout.type";
 import { useAuth } from "../context/auth";
 
 interface Props extends FormikProps<CheckoutInfo> {
-  onNext: () => void
+  onNext: () => void;
 }
 
-export const Login = ({ touched, errors, values, setFieldTouched, setFieldValue, onNext }: Props) => {
-  const totalAmount = useMemo(() => values.cost ? (Number(values.cost) + Number(values.cost) * Number(values.tipPercent || 0) / 100) : 0, [values])
-  const isRequiredLogin = useMemo(() => totalAmount >= 500, [totalAmount])
+export const Login = ({
+  touched,
+  errors,
+  values,
+  setFieldTouched,
+  setFieldValue,
+  onNext,
+}: Props) => {
+  const totalAmount = useMemo(
+    () =>
+      values.cost
+        ? Number(values.cost) +
+          (Number(values.cost) * Number(values.tipPercent || 0)) / 100
+        : 0,
+    [values]
+  );
+  const isRequiredLogin = useMemo(() => totalAmount >= 500, [totalAmount]);
 
-  const isValid = useMemo(() =>
-    !errors.email &&
-    !errors.password &&
-    values.email &&
-    values.password
-    , [values, errors])
+  const isValid = useMemo(
+    () => !errors.email && !errors.password && values.email && values.password,
+    [values, errors]
+  );
 
   const { onLogin } = useAuth();
 
   const onSubmit = () => {
     if (!isValid) {
-      return
+      return;
     }
 
-    onLogin(values.email, values.password as string)
-  }
+    onLogin(values.email, values.password as string);
+  };
 
-  return <div className='widget-container flex flex-col'>
-    <h3 className="text-white text-4xl mb-10 text-center">Login</h3>
-    <div className="flex-1 flex flex-col items-center justify-center">
-      {isRequiredLogin && <div className="text-center text-purple-600 mb-5">Required an account for purchasing over $500</div>}
-      <div className="flex flex-col justify-center w-full">
-        <input
-          value={values.email}
-          onBlur={() => setFieldTouched('email', true)}
-          onChange={(e) => setFieldValue('email', e.target.value)}
-          className="text-white text-lg outline-none bg-white/20 pl-2 pr-2 w-full h-7 shadow-sm border-l-2 border-b-2 border-white rounded-sm placeholder-white"
-          placeholder="Email"
-        />
-        {touched.email && errors.email && <div className='text-red-400 text-[12px] text-left'>{errors.email}</div>}
+  return (
+    <div className="widget-container flex flex-col">
+      <h3 className="text-white text-4xl mb-10 text-center">Login</h3>
+      <div className="flex-1 flex flex-col items-center justify-center">
+        {isRequiredLogin && (
+          <div className="text-center text-purple-600 mb-5">
+            Required an account for purchasing over $500
+          </div>
+        )}
+        <div className="flex flex-col justify-center w-full">
+          <input
+            value={values.email}
+            onBlur={() => setFieldTouched("email", true)}
+            onChange={(e) => setFieldValue("email", e.target.value)}
+            className="text-white text-lg outline-none bg-white/20 pl-2 pr-2 w-full h-7 shadow-sm border-l-2 border-b-2 border-white rounded-sm placeholder-white"
+            placeholder="Email"
+          />
+          {touched.email && errors.email && (
+            <div className="text-red-400 text-[12px] text-left">
+              {errors.email}
+            </div>
+          )}
+        </div>
+        <div className="flex flex-col justify-center mt-3 w-full">
+          <input
+            value={values.password}
+            onBlur={() => setFieldTouched("password", true)}
+            onChange={(e) => setFieldValue("password", e.target.value)}
+            className="text-white text-lg outline-none bg-white/20 pl-2 pr-2 w-full h-7 shadow-sm border-l-2 border-b-2 border-white rounded-sm placeholder-white"
+            type="password"
+            placeholder="Password"
+          />
+          {touched.password && !values.password && (
+            <div className="text-red-400 text-[12px] text-left">
+              Password is required
+            </div>
+          )}
+        </div>
+        <div className="text-white mt-5">
+          Don't have an account?{" "}
+          <span
+            className="cursor-pointer text-purple-600"
+            onClick={() => setFieldValue("auth", "signup")}
+          >
+            Sign up
+          </span>
+        </div>
+        <div className="text-white border-white border-solid border-[1px] p-2 rounded mt-5 text-justify text-xs">
+          <b>US Patriot Act</b>: To help the government fight the funding of
+          terrorism and money laundering activities, federal law requires all
+          financial institutions to obtain, verify, and record information that
+          identifies each person who opens an account. What this means for you:
+          When you open an account, we will ask for your name, address, date of
+          birth, and other information that will allow us to identify you. We
+          will require your driver's license or other identifying documentation.
+        </div>
       </div>
-      <div className="flex flex-col justify-center mt-3 w-full">
-        <input
-          value={values.password}
-          onBlur={() => setFieldTouched('password', true)}
-          onChange={(e) => setFieldValue('password', e.target.value)}
-          className="text-white text-lg outline-none bg-white/20 pl-2 pr-2 w-full h-7 shadow-sm border-l-2 border-b-2 border-white rounded-sm placeholder-white"
-          type="password"
-          placeholder="Password"
-        />
-        {touched.password && !values.password && <div className='text-red-400 text-[12px] text-left'>Password is required</div>}
-      </div>
-      <div className="text-white mt-5">
-        Don't have an account? <span className="cursor-pointer text-purple-600" onClick={() => setFieldValue('auth', 'signup')}>Sign up</span>
-      </div>
-    </div>
-    {!isRequiredLogin && (
+      {!isRequiredLogin && (
+        <button
+          onClick={onNext}
+          className={`mt-4 text-white text-lg text-center w-full rounded-md h-11 border-2 border-white flex items-center justify-center shadow-md shadow-white bg-gradient-to-b from-purple-400 to-purple-600`}
+        >
+          Skip
+        </button>
+      )}
       <button
-        onClick={onNext}
-        className={`mt-4 text-white text-lg text-center w-full rounded-md h-11 border-2 border-white flex items-center justify-center shadow-md shadow-white bg-gradient-to-b from-purple-400 to-purple-600`}
+        onClick={() => onSubmit()}
+        className={`mt-4 text-white text-lg text-center w-full rounded-md h-11 border-2 border-white flex items-center justify-center shadow-md shadow-white ${
+          isValid ? "bg-gradient-to-b from-purple-400 to-purple-600" : ""
+        }`}
       >
-        Skip
+        <div className="flex items-center">Login</div>
       </button>
-    )}
-    <button
-      onClick={() => onSubmit()}
-      className={`mt-4 text-white text-lg text-center w-full rounded-md h-11 border-2 border-white flex items-center justify-center shadow-md shadow-white ${isValid ? 'bg-gradient-to-b from-purple-400 to-purple-600' : ''}`}
-    >
-      <div className="flex items-center">
-        Login
-      </div>
-    </button>
-  </div>
-}
+    </div>
+  );
+};
